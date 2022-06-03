1ac56bdcbcb4ebfa4d8e5d49cd12b1aef7e4aebe
steep_sama <stevesalvadorman@gmail.com>
2022-06-03 09:47:59 -0500

    Nuevas modificaciones

--- src/java/com/steep/model/Rol.java
+++ src/java/com/steep/model/Rol.java
@@ -9,5 +9,32 @@ package com.steep.model;
  * @author Steep Salvador
  */
 public class Rol {
-    
+
+    private int id;
+    private String nombre;
+
+    public Rol() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
 }
